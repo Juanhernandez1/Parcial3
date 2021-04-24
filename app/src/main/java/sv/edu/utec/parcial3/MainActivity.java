916588dd2fdb4cb0aa441466ad04111daf9c45e6
Juanhernandez1 <2544692017@mail.utec.edu.sv>
2021-04-23 23:17:03 -0600

    📝 boton

--- app/src/main/java/sv/edu/utec/parcial3/MainActivity.java
+++ app/src/main/java/sv/edu/utec/parcial3/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,32 +18,36 @@ public class MainActivity extends AppCompatActivity {
 
     private AdaptorComment adaptorComment;
     private RecyclerView RecyclerViewComment;
-
+    private Button Consultar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Call<CommentsResponse> call = RetrofitService.service.getComments();
 
         RecyclerViewComment = findViewById(R.id.rcnombres);
-
-        Call<CommentsResponse> call = RetrofitService.service.getComments();
-        call.enqueue(new Callback<CommentsResponse>() {
-            @Override
-            public void onResponse(Call<CommentsResponse> call, Response<CommentsResponse> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    Log.i("MainActivity", "onResponse: " + response.body().getComments().get(0).getName());
-                    adaptorComment = new AdaptorComment(response.body().getComments());
-                    RecyclerViewComment.setAdapter(adaptorComment);
-                } else {
-                    Log.i("MainActivity", "onResponse: Failed");
+        Consultar = findViewById(R.id.button);
+        Consultar.setOnClickListener(v -> {
+            call.enqueue(new Callback<CommentsResponse>() {
+                @Override
+                public void onResponse(Call<CommentsResponse> call, Response<CommentsResponse> response) {
+                    if (response.isSuccessful() && response.body() != null) {
+                        Log.i("MainActivity", "onResponse: " + response.body().getComments().get(0).getName());
+                        adaptorComment = new AdaptorComment(response.body().getComments());
+                        RecyclerViewComment.setAdapter(adaptorComment);
+                    } else {
+                        Log.i("MainActivity", "onResponse: Failed");
+                    }
                 }
-            }
 
-            @Override
-            public void onFailure(Call<CommentsResponse> call, Throwable t) {
-                Log.e("MainActivity", "Call failed", t);
-            }
+                @Override
+                public void onFailure(Call<CommentsResponse> call, Throwable t) {
+                    Log.e("MainActivity", "Call failed", t);
+                }
+            });
         });
+
+
     }
 }
